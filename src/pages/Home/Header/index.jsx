0338a52440413fa8c8cd7a198e8be9e41855818d
Admin <admin@MacBook-Pro-cua-Admin.local>
2023-08-07 20:26:34 +0700

    Update

--- src/pages/Home/Header/index.jsx
+++ src/pages/Home/Header/index.jsx
@@ -1,16 +1,42 @@
-import { Col, Row, Input, Button } from 'antd';
+import { Col, Row, Input, Button, Breadcrumb } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import ProjectModal from '../NewProjectModal';
 import TicketModal from '../AddNewTicketModal';
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import clsx from 'clsx';
 
 function Header() {
   const [modalPrOpen, setOpenPrModal] = useState(false);
   const [modalTiOpen, setOpenTiModal] = useState(false);
+  const [breadcrumbOptions, setBreadcrumbOptions] = useState([
+    {
+      title: <Link to={`/`}>All Projects</Link>,
+    },
+  ]);
 
-  let { projectId, ticketId } = useParams();
+  // {
+  //   title: <Link to={`/${projectId}`}>Application Center</Link>,
+  // },
+  // {
+  //   title: <Link to={`/${projectId}/${ticketId}`}>Application List</Link>,
+  // },
+
+  const { projectId, ticketId } = useParams();
+
+  useEffect(() => {
+    if (projectId) {
+      setBreadcrumbOptions([...breadcrumbOptions, { title: <Link to={`/${projectId}`}>Application Center</Link> }]);
+    }
+    if (projectId && ticketId) {
+      setBreadcrumbOptions([
+        ...breadcrumbOptions,
+        { title: <Link to={`/${projectId}`}>Application Center</Link> },
+        { title: <Link to={`/${projectId}/${ticketId}`}>Application List</Link> },
+      ]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [projectId, ticketId]);
 
   function onAddNewPr() {
     if (projectId) {
@@ -23,7 +49,9 @@ function Header() {
   return (
     <>
       <Row className={clsx('flex', 'content-center', 'mb-10')}>
-        <Col span={17}>All Project</Col>
+        <Col span={17}>
+          <Breadcrumb items={breadcrumbOptions} />
+        </Col>
         {!ticketId ? (
           <>
             <Col span={4} className=''>
--- src/pages/TicketInfo/index.jsx
+++ src/pages/TicketInfo/index.jsx
@@ -1,37 +1,79 @@
-import { Form, Row, Button, Space } from 'antd';
-import { ShareAltOutlined, InfoCircleOutlined } from '@ant-design/icons';
+import { Form, Row, Button, Space, Typography, Col, Upload, Input } from 'antd';
+import {
+  ShareAltOutlined,
+  InfoCircleOutlined,
+  MinusCircleOutlined,
+  LoadingOutlined,
+  PlusOutlined,
+} from '@ant-design/icons';
 import { useStores } from '../../stores';
 import { useParams } from 'react-router-dom';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { getObjectFromProxy } from '../../utils/common';
+// import { UploadOutlined } from '@ant-design/icons';
+import UploadImg from '../Home/AddNewTicketModal/UploadImg';
 
 function TicketInfo() {
-  const labelForm = (
-    <>
-      Images <Button>Upload</Button>
-    </>
+  const [imageList, setImageList] = useState();
+  const { ticketStore } = useStores();
+  const { projectId, ticketId } = useParams();
+  const data = useMemo(() => ticketStore.getTicketInfo(projectId, ticketId), [ticketStore, projectId, ticketId]);
+  console.log('All data', getObjectFromProxy(data));
+  const { ticketName, images, storyList } = data;
+
+  console.log('storyList', storyList);
+
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState();
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
+      </div>
+    </div>
   );
 
-  const { projectId, ticketId } = useParams();
+  const getBase64 = (img, callback) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+  };
 
-  const { ticketStore } = useStores();
+  const handleChange = (info) => {
+    getBase64(info.file.originFileObj, (url) => {
+      setLoading(false);
+      setImageUrl(url);
+    });
+  };
 
-  const data = useMemo(() => ticketStore.getTicketInfo(projectId, ticketId), [ticketStore, projectId, ticketId]);
-  console.log('data', getObjectFromProxy(data));
+  const normFile = (e) => {
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e?.fileList;
+  };
+
+  useEffect(() => {
+    setImageList(images);
+  }, [images]);
+
+  console.log(getObjectFromProxy(storyList));
 
   return (
     <>
-      <Form
-        layout='vertical'
-        // initialValues={}
-      >
+      <Form layout='vertical' initialValues={{ ...data }}>
         <Row className='flex flex-row justify-between content-center'>
-          <Row span={24} className='content-center'>
-            <Space className='content-center'>
-              <h3>Ticket Info</h3>
-              <ShareAltOutlined />
-              <InfoCircleOutlined />
-            </Space>
+          <Row span={24} className='content-center' align='middle' justify='space-around'>
+            <Row className='flex content-center '>
+              <Typography.Title level={3}>{ticketName}</Typography.Title>
+              <ShareAltOutlined className='text-2xl ml-3' />
+              <InfoCircleOutlined className='text-2xl ml-3' />
+            </Row>
           </Row>
           <Row span={24}>
             <Space>
@@ -40,11 +82,83 @@ function TicketInfo() {
             </Space>
           </Row>
         </Row>
-        <span>{`Ticket description for "Create Login page" ticket`}</span>
+        <Typography.Title level={5}>{`Ticket description for ${ticketName} ticket`}</Typography.Title>
         <div className='border-2 border-black rounded'>
-          <Form.Item name='images' label={labelForm}></Form.Item>
-          <Form.Item name='storiesStore' label='Storires'></Form.Item>
-          <Button>Add User Story</Button>
+          <Form.Item name='images' label='Images'>
+            <UploadImg fileList={imageList} setFileList={setImageList} lengthList={8} />
+          </Form.Item>
+          <Form.Item name='storyList' label='Storires'>
+            <Form.List name='storyList'>
+              {(fields, { add, remove }) => (
+                <>
+                  {fields.map(
+                    ({ key, name, ...restField }) =>
+                      console.log('zz', getObjectFromProxy(data.storyList[key].storyImage[0])) || (
+                        <Row key={key} className='mb-6' align='middle'>
+                          <Col className='w-7/12 h-full'>
+                            <Form.Item
+                              {...restField}
+                              name={[name, 'storyDescription']}
+                              rules={[
+                                {
+                                  required: true,
+                                  message: 'Missing story description',
+                                },
+                              ]}
+                              className='mb-0'
+                            >
+                              <Input.TextArea classNames='' placeholder='Story descriptions' rows='4' />
+                            </Form.Item>
+                          </Col>
+
+                          <Col className='w-1/12' />
+
+                          <Col className='w-1/12'>
+                            <Form.Item
+                              {...restField}
+                              name={[name, 'storyImage']}
+                              valuePropName='fileList'
+                              getValueFromEvent={normFile}
+                              className='mb-0'
+                            >
+                              <Upload
+                                name='avatar'
+                                listType='picture-card'
+                                fileList={imageList}
+                                className='avatar-uploader'
+                                showUploadList={false}
+                                action='https://www.mocky.io/v2/5cc8019d300000980a055e76'
+                                onChange={handleChange}
+                              >
+                                {imageUrl ? (
+                                  <img
+                                    src={imageUrl}
+                                    alt='avatar'
+                                    style={{
+                                      width: '100%',
+                                    }}
+                                  />
+                                ) : (
+                                  uploadButton
+                                )}
+                              </Upload>
+                            </Form.Item>
+                          </Col>
+                          <Col className='w-1/12'>
+                            <MinusCircleOutlined onClick={() => remove(name)} />
+                          </Col>
+                        </Row>
+                      ),
+                  )}
+                  <Form.Item>
+                    <Button type='dashed' onClick={() => add()} icon={<PlusOutlined />}>
+                      Add new user story
+                    </Button>
+                  </Form.Item>
+                </>
+              )}
+            </Form.List>
+          </Form.Item>
         </div>
       </Form>
     </>
